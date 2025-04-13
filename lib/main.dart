44e0467f2ebaaaa3370eabdb0c2ff49b0bb28706
Mahbub1136 <mahbubrahman09057666@gmail.com>
2025-04-14 01:15:22 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Pritom's GYM Center",
+      title: "Mahbub's GYM Center",
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
